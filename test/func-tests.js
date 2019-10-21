63f74705797a92dd81b30ebdab93a7686bca5dee
drmeloy <drmeloy@gmail.com>
2019-10-20 19:27:00 -0600

    fixed travis error

--- test/func-tests.js
+++ test/func-tests.js
@@ -1,7 +1,5 @@
 import { hasCompletedAllQuests } from '../map/has-completed-all-quests.js';
 import { quests } from '../data/quest-data.js';
-import { scoreQuest } from '../quest/score-quest.js';
-import { findById } from '../common/find-by-id.js';
 
 const test = QUnit.test;
 
@@ -19,28 +17,3 @@ test('hasCompletedAllQuests returns true if all quests have been completed', fun
     const expected = true;
     assert.equal(result, expected);
 });
-
-// test('scoreQuest properly updates user state', function(assert) {
-//     const user = {
-//         hp: 100,
-//         social: 0,
-//         items: {},
-//         completed: {}
-//     };
-//     const questsArray = quests;
-//     const choiceId = 'break-out';
-//     const choice = findById(questsArray.choices, choiceId);
-//     const questId = questsArray.escape.id;
-
-//     const expected = {
-//         hp: 70,
-//         social: -10,
-//         items: {},
-//         completed: {
-//             escape: true
-//         }
-//     };
-
-//     const test = scoreQuest(choice, questId, user);
-//     assert.equal(test, expected);
-// });
